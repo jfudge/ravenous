352521b250eb33948c2a2553ee823f82a5ceb67f
j <123@gmail.com>
2020-08-20 17:44:39 -0700

    fix searchbar

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './BusinessList.css';
-
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        {
-          this.props.businesses.map(business => {
-            return <Business business={business} key={business.id} />
-          })
-        }
+        {this.props.businesses.map((business => {
+          return <Business key={business.id} business={business} />;
+        }))}
       </div>
-    );
+    )
   }
 }
 
-export default BusinessList;
+export default BusinessList
--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,16 +1,17 @@
-const apiKey = '9HKRzxwyRS6hNaa15BPUOKSUNnAARqJ6AJCa0QJdusbzomqGIvlErZxnFnkNJTpi0OoO-Kt20ntBaYXrhC9_lJbNCY-UOAPEV-xZ9EUQA3zWMa0CoQ5EfSnIsXjfW3Yx';
+const apiKey = 'UBsXQP308WyYSUQ7rdto3fi1ZqCIQsxTZJpJ0gdxL83akL9-3-REhBPc7w8dcrYAY2YwVZjGHuUgPeoefbrEi4cNxi-AuMEcRYtxJCcvl3_RwAC7xZ5r1zntpbIyXXYx';
 
 const Yelp = {
-    search(term, location, sortBy) {
-      return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
-        headers: {
-          Authorization: `Bearer ${apiKey}`
-        }
-      }).then(response => {
-        return response.json();
-      }).then(jsonResponse => {
-        if (jsonResponse.businesses) {
-          return jsonResponse.businesses.map(business => ({
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    })
+    .then(response => response.json())
+    .then((jsonResponse) => {
+      if(jsonResponse.businesses){
+        return jsonResponse.businesses.map((business) => {
+          return {
             id: business.id,
             imageSrc: business.image_url,
             name: business.name,
@@ -21,11 +22,11 @@ const Yelp = {
             category: business.categories[0].title,
             rating: business.rating,
             reviewCount: business.review_count
-          }));
-        }
-      });
-    }
-  };
-  
-  export default Yelp;
-  
+          }
+        })
+      }
+    })
+  }
+};
+
+export default Yelp;
